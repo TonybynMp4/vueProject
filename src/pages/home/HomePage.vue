30047df64a20252715e8961ed96e0f6eea087174
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-03-25 18:25:16 +0100

    Refactor HomePage.vue: restructure template and enhance product display logic

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
-    import { ref } from 'vue';
+	import Product from '@/components/ProductComponent.vue';
+	import useAuthStore from '@/stores/auth-store';
+	import useDbStore from '@/stores/fakeDb-store';
+	import { computed, } from 'vue';
+
+	const authStore = useAuthStore();
+    const isLoggedIn = computed(() => authStore.getUser !== null);
+	const dbStore = useDbStore();
+	const products = computed(() => dbStore.getProducts);
 </script>
 
 <template>
     <main>
         <h1>Bienvenue sur ce site ecommerce de fou</h1>
-        <p>Vous pouvez vous connecter:</p>
-        <router-link class="button" to="/login">Se connecter</router-link>
-        <p>Ou, si ce n'est pas déjà fait, vous inscrire:</p>
-        <router-link class="button" to="/register">S'inscrire</router-link>
+		<section v-if="!isLoggedIn">
+			<p>Vous pouvez vous connecter:</p>
+			<router-link class="button" to="/login">Se connecter</router-link>
+			<p>Ou, si ce n'est pas déjà fait, vous inscrire:</p>
+			<router-link class="button" to="/register">S'inscrire</router-link>
+		</section>
+		<section class="productList" v-else>
+			<Product v-for="product in products" :key="product.id" :product="product" />
+		</section>
     </main>
 </template>
 
@@ -18,5 +31,15 @@
         flex-direction: column;
         gap: 1rem;
         align-items: center;
+		width: 80%;
+		margin: 0 auto;
     }
+
+	.productList {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1rem;
+		justify-content: center;
+		width: 100%;
+	}
 </style>
